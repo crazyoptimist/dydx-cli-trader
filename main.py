bc6a7fa6f5eda8dbf491c646d5edb75bdc26a422
crazyoptimist <crazyoptimist@mail.com>
2021-12-02 17:42:46 +0100

    make the game configurable in the env file

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from dydx3.constants import API_HOST_MAINNET, NETWORK_ID_MAINNET, \
         ORDER_SIDE_BUY, ORDER_SIDE_SELL, \
         ORDER_TYPE_LIMIT, ORDER_TYPE_TRAILING_STOP, ORDER_TYPE_TAKE_PROFIT, \
         ORDER_STATUS_OPEN, ORDER_STATUS_PENDING, ORDER_STATUS_FILLED, ORDER_STATUS_UNTRIGGERED, \
-        POSITION_STATUS_OPEN, POSITION_STATUS_CLOSED, \
-        MARKET_BTC_USD, MARKET_ETH_USD, MARKET_AVAX_USD, MARKET_ADA_USD, MARKET_MATIC_USD
+        POSITION_STATUS_OPEN, POSITION_STATUS_CLOSED
 from web3 import Web3
 import pandas as pd
 import numpy as np
@@ -18,11 +17,12 @@ load_dotenv()
 WEB3_PROVIDER_URL = 'https://mainnet.infura.io/v3/' + os.getenv('INFURA_PROJECT_ID')
 # multiply of standard deviation
 DEVIATION_THRESHOLD = 2.5
-TARGET_MARKET = MARKET_MATIC_USD
-ORDER_SIZE = '100'
-TRAILING_PERCENT = 1
-PRICE_DELTA = 0.015
-CANDLE_RESOLUTION = '1MIN'
+CANDLE_RESOLUTION = '1HOUR'
+TARGET_MARKET = os.getenv('TARGET_MARKET')
+ORDER_SIZE = os.getenv('ORDER_SIZE')
+ORDER_SIDE = os.getenv('ORDER_SIDE')
+TRAILING_PERCENT = 5
+PRICE_DELTA = 0.055
 
 
 api_key_credentials = {
@@ -56,7 +56,6 @@ class DydxData:
         mean = self.df['close'].mean()
         std = np.std(self.df['close'])
         z_score = (self.df['close'].iloc[0] - mean)/std
-        print(self.df['startedAt'].iloc[0], " -- ", z_score)
         return z_score
 
 
@@ -80,10 +79,7 @@ def job():
         'limit_fee': 0.001,
         'expiration_epoch_seconds': int(time.time()) + 4 * 7 * 1440 * 60,
     }
-    if not (z_score > DEVIATION_THRESHOLD or z_score < -DEVIATION_THRESHOLD):
-        print("Deviation does not exceed the limit")
-        return
-    elif z_score > DEVIATION_THRESHOLD:
+    if ORDER_SIDE == 'BUY':
         buy_order_params = {
             **order_params,
             'order_type': ORDER_TYPE_LIMIT,
@@ -100,7 +96,7 @@ def job():
         }
         place_order(sell_order_params)
         return
-    elif z_score < -DEVIATION_THRESHOLD:
+    elif ORDER_SIDE == 'SELL':
         sell_order_params = {
             **order_params,
             'order_type': ORDER_TYPE_LIMIT,
@@ -118,3 +114,5 @@ def job():
         place_order(stop_order_params)
         return
 
+
+job()
